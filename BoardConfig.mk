6e222de5813e6b885073e3a0e145362f44484077
AnClark <clarklaw4701@qq.com>
2017-02-14 12:40:37 +0000

    Make QSEECOMD work properly
    Change-Id: I679c381cf566379e5685f17d08ca27d80adea757

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,14 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-VENDOR_PATH := device/xiaomi/helium
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8956
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8956
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Architecture
 TARGET_ARCH := arm64
@@ -31,58 +38,35 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
 
-TARGET_CPU_CORTEX_A53 := true
-
-TARGET_BOARD_PLATFORM := msm8952
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
-
-TARGET_USES_64_BIT_BINDER := true
-ENABLE_CPUSETS := true
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := msm8952
-TARGET_NO_BOOTLOADER := true
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk ramoops_memreserve=4M
+BOARD_KERNEL_CMDLINE := 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk ramoops_memreserve=4M'
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
-TARGET_PREBUILT_KERNEL := $(VENDOR_PATH)/prebuilt/Image.gz-dtb
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-# Display
-BOARD_USES_ADRENO := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-SF_VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-TARGET_USES_OVERLAY := true
-USE_OPENGL_RENDERER := true
-VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
-
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
-
-OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/helium/mkbootimg.mk
+TARGET_PREBUILT_KERNEL := device/xiaomi/helium/kernel
 
 # Encryption
+# Switch on encryption support here.
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Filesystem
+# Keymaster Setting
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Partitions
+
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x4000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x4000000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
@@ -91,58 +75,26 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
-# Init
-#TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-#TARGET_RECOVERY_DEVICE_MODULES := libinit_msm
-
-# Keymaster
-TARGET_PROVIDES_KEYMASTER := true
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
-# Lights
-BOARD_LIGHTS_VARIANT := aw2013
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Media
-TARGET_USES_MEDIA_EXTENSIONS := true
-
-# Peripheral manager
-TARGET_PER_MGR_ENABLED := true
-
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-
-# Qualcomm
-#BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QC_TIME_SERVICES := true
-
 # Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := device/xiaomi/helium/recovery.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-# RIL
-PROTOBUF_SUPPORTED := true
-TARGET_RIL_VARIANT := caf
-
-# Sensors
-USE_SENSOR_MULTI_HAL := true
-
-# TWRP specific build flags
-RECOVERY_SDCARD_ON_DATA := true
+# TWRP
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/module/g_android/parameters/file"
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_DEFAULT_BRIGHTNESS := 80
-TW_EXCLUDE_SUPERSU := true
-TW_EXTRA_LANGUAGES := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+#TW_TARGET_USES_QCOM_BSP := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_NTFS_3G := true
-TW_THEME := portrait_hdpi
+TW_THEME= portrait_hdpi
 
-# Logcat
+# Vendor Init
+# Maybe this is an essential for enabling decryption.
+TARGET_INIT_VENDOR_LIB := libinit_msm
+
+# Enable logcat support! Yahoo!
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
